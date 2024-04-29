b247a205298723fbe1ee95a4ac4e817a623c1624
LATY.OUN <LATY.OUN@PNC.LAN>
2024-04-29 16:28:13 +0700

    #15 write code in file gate

--- AirlineManagerment/Flight/Seat.ts
+++ AirlineManagerment/Flight/Seat.ts
@@ -1,11 +1,11 @@
-export class Seat{
-    private seatNumber: number;
+// export class Seat{
+//     private seatNumber: number;
 
-    constructor(seatNumber: number) {
-        this.seatNumber = seatNumber;
-    }
+//     constructor(seatNumber: number) {
+//         this.seatNumber = seatNumber;
+//     }
 
-    public getSeatNumber(): number {
-        return this.seatNumber;
-    }
-}
+//     public getSeatNumber(): number {
+//         return this.seatNumber;
+//     }
+// }
