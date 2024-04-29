5e986b646cdd6b18d42453b4147c7821cacd7c2b
Tiv-Nguot <tiv.nguot@student.passerellesnumeriques.org>
2024-04-29 18:35:57 +0700

    create class passenger and test it

--- AirlineManagerment/Person/Passenger.ts
+++ AirlineManagerment/Person/Passenger.ts
@@ -1,1 +1,33 @@
-//TODO:
+import { Person } from "./Person";
+import { Gender } from "./Gender";
+
+export class Passenger extends Person {
+    constructor(
+        name: string, 
+        gender: Gender, 
+        dateOfBirth: string,
+        nationality:string,
+    private  email:string,
+    private phoneNumber:string,
+    private address:string
+    // private BoardingPass: BoardingPass 
+    ) {
+        super(name, gender, dateOfBirth, nationality);
+    }
+
+    getEmail():string{
+        return this.email;
+    }
+
+    getPhoneNumber():string{
+        return this.phoneNumber;
+    }
+    
+    getAddress():string{
+        return this.address;
+    }
+    getContacts():string{
+        return `Email: ${this.getEmail()}, Phone Number: ${this.getPhoneNumber()}, Address: ${this.getAddress()}`;
+    }
+
+}
