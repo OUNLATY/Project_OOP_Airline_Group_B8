8c6c93db2ac7532aeeeff60260346643ed83a7e4
LATY.OUN <LATY.OUN@PNC.LAN>
2024-04-29 15:19:35 +0700

    [feat] create branch of Seat #13

---
+++ AirlineManagerment/Flight/Seat.ts
@@ -0,0 +1,11 @@
+export class Seat{
+    private seatNumber: number;
+
+    constructor(seatNumber: number) {
+        this.seatNumber = seatNumber;
+    }
+
+    public getSeatNumber(): number {
+        return this.seatNumber;
+    }
+}
