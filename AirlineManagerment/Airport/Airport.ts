4ea6b01e81693a44107140852543cc9f35eaec17
LATY.OUN <LATY.OUN@PNC.LAN>
2024-04-29 14:16:18 +0700

    [feat] create code Airport #9

--- AirlineManagerment/Airport/Airport.ts
+++ AirlineManagerment/Airport/Airport.ts
@@ -1,1 +1,38 @@
 //TODO:
+
+//TODO:
+
+
+
+export class Airport {
+    private name: string;
+    private codeAirport: string;
+    private location: string;
+    private country: string;
+    
+
+    constructor(name: string, codeAirport: string, location: string, country: string) {
+        this.name = name;
+        this.codeAirport = codeAirport;
+        this.location = location;
+        this.country = country;
+        
+    }
+
+    getAirportName() {
+        return this.name;
+    }
+    getAirportCode() {
+        return this.codeAirport;
+    }
+    getAirportLocation() {
+        return this.location;
+    }
+    getAirportCountry() {
+        return this.country;
+    }
+
+    
+
+    
+}
