ddf4657578c45b6afdba1a1407e938ecb272f7b7
Tiv-Nguot <tiv.nguot@student.passerellesnumeriques.org>
2024-04-30 13:48:15 +0700

    #19 create and test the class Department

--- Main.ts
+++ Main.ts
@@ -1,8 +1,13 @@
 import {Person} from "./AirlineManagerment/Person/Person";
 import { Gender } from "./AirlineManagerment/Person/Gender";
 import {Passenger} from "./AirlineManagerment/Person/Passenger";
+import {Department} from "./AirlineManagerment/Department/Department";
+import { Airline } from "./AirlineManagerment/Airline/Airline.ts";
 
 // let passenger1 = new Passenger("passenger1",Gender.FEMALE,"2-2-2020","Cambodian","pass@gmail.com","097-879-9055","Phnom Penh");
 // console.log('passenger1', passenger1.getContacts());
 
+// let airline1 = new Airline("airline1","Cambodia","+123G")
+// let department1 = new Department("department1",airline1)
+// console.log('department1', department1)
 
