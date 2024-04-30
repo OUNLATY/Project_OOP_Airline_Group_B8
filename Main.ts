4154661b11fb470ed32fc88143998144a5f822b2
Tiv-Nguot <tiv.nguot@student.passerellesnumeriques.org>
2024-04-30 14:56:34 +0700

    #23 create and test the class Pilot

--- Main.ts
+++ Main.ts
@@ -2,6 +2,7 @@ import {Person} from "./AirlineManagerment/Person/Person";
 import { Gender } from "./AirlineManagerment/Person/Gender";
 import {Passenger} from "./AirlineManagerment/Person/Passenger";
 import {Employee} from "./AirlineManagerment/Employee/Employee";
+import {Pilot} from "./AirlineManagerment/Employee/Pilot";
 import {Department} from "./AirlineManagerment/Department/Department";
 import { Airline } from "./AirlineManagerment/Airline/Airline.ts";
 
@@ -12,3 +13,5 @@ import { Airline } from "./AirlineManagerment/Airline/Airline.ts";
 // let department1 = new Department("department1",airline1)
 // console.log('department1', department1)
 
+// let pilot = new Pilot("Pilot1",Gender.FEMALE,"2-2-2020","Cambodian","pass@gmail.com","097-879-9055",department1,900)
+// console.log('pilot', pilot.getSalary()+ "$")
